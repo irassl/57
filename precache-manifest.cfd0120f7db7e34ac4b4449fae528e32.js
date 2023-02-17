4ac272b6c100433e3e02a1ac3290dc53739a5d9b
irassl <smith.ya-s@yandex.ru>
2023-02-17 09:46:31 +0300

    Updates

---
+++ precache-manifest.cfd0120f7db7e34ac4b4449fae528e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6992a7c11aa19d4ce11a9a96bd37331",
+    "url": "/57/index.html"
+  },
+  {
+    "revision": "881e7b5c989e9abb74a2",
+    "url": "/57/static/css/main.94d872b4.chunk.css"
+  },
+  {
+    "revision": "97072953f3f3322df47a",
+    "url": "/57/static/js/2.adbaf3ca.chunk.js"
+  },
+  {
+    "revision": "881e7b5c989e9abb74a2",
+    "url": "/57/static/js/main.023e4c3d.chunk.js"
+  },
+  {
+    "revision": "e15130e2e526d74eb5a7",
+    "url": "/57/static/js/runtime~main.64eaa930.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/57/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/57/static/media/user.8b1dbb1f.png"
+  }
+]);
